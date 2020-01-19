918235789aaed62cc9f53ae25a85611e771403f8
Wesley Chen <weschen1996@gmail.com>
2020-01-19 23:00:39 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,8 +10,8 @@ def get_japanese_emoticon(emot)
     lib = load_library('./data.yml')
     i = 0
     while i < lib.keys.length
-        if lib[lib.keys[i]].include? emot
-            return lib[lib.keys[i]][2]
+        if lib[lib.keys[i]][0] == emot
+            return lib[lib.keys[i]][1]
         end
         i+= 1
     end
